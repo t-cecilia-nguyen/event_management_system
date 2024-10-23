b4f94c28a0a6a3c5a9ec6c455851a743cf46bff4
Cecilia Nguyen <cecilia.t.nguyen@gmail.com>
2024-10-23 19:26:57 -0400

    created microservice-parent

---
+++ microservice-parent/src/main/java/com/example/microserviceparent/MicroserviceParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceParentApplication.class, args);
+    }
+
+}
